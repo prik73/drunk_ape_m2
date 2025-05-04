ca99e12a9f9906c737d118e5fa5f426c57aefddf
prik73 <prinovac@gmail.com>
2025-05-04 21:01:40 +0530

    Remove .env.local from version control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { SignedOut, SignInButton, UserButton,SignedIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      hello
-      <Button>Click me</Button>
+    <div className="m-10">
+      home page
     </div>
   );
 }
+ 
