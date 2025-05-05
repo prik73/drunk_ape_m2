2630e6687fc1f7078efe42224936fbdaaba03657
prik73 <prinovac@gmail.com>
2025-05-05 09:44:57 +0530

    verification of convex clerk webhooks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import {ClerkProvider} from '@clerk/nextjs'
 import localFont from "next/font/local";
 import "./globals.css";
+import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,12 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ConvexClerkProvider>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
-  </ClerkProvider>
+  </ConvexClerkProvider>
   );
 }
