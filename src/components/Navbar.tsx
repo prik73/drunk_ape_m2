d60dd03f5b5be94e055c690b163d22ea68feaf38
prik73 <prinovac@gmail.com>
2025-05-10 09:59:58 +0530

    removed local storage code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 import { CodeIcon } from "lucide-react";
@@ -9,18 +8,18 @@ function Navbar() {
   return (
     <nav className="border-b">
       <div className="flex h-16 items-center px-4 container mx-auto">
-        {/* left side waala logo */}
+        {/* LEFT SIDE -LOGO */}
         <Link
           href="/"
           className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
           <CodeIcon className="size-8 text-emerald-500" />
           <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
-            Code-Collab
+            CodeSync
           </span>
         </Link>
 
-        {/* actions of right side */}
+        {/* RIGHT SIDE - ACTIONS */}
         <SignedIn>
           <div className="flex items-center space-x-4 ml-auto">
             <DashboardBtn />
